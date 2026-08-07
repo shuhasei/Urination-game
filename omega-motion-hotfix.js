75e89e9deddd0f35196717caa201d07884238aa9
shuhasei <shuhasei71@gmail.com>
2026-08-07 21:59:00 +0900

    Animate Omega Flowey body and TV face from user assets

---
+++ omega-motion-hotfix.js
@@ -0,0 +1,4 @@
+(() => {
+  'use strict';
+  const MOTION_VERSION = '20260807-omega-motion1';
+  const TV_NORMAL = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAASwAAADwCAYAAAAb1HMxAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAA...
