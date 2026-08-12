c99943f4fe41115ae629faaa0924999ce086cb28
shuhasei <shuhasei71@gmail.com>
2026-08-13 00:23:56 +0900

    Chain Sans video fidelity v23 after v22

---
+++ sans-v23-chain.js
@@ -0,0 +1,28 @@
+(() => {
+  'use strict';
+
+  let rawV22 = null;
+  let chained = null;
+
+  Object.defineProperty(window, 'applySansRecursionMediaFixV22', {
+    configurable: true,
+    enumerable: true,
+    get() { return chained || rawV22; },
+    set(value) {
+      rawV22 = value;
+      if (typeof value !== 'function') {
+        chained = value;
+        return;
+      }
+      chained = source => {
+        let result = value(source);
+        if (typeof window.applySansVideoFidelityV23 === 'function') {
+          result = window.applySansVideoFidelityV23(result);
+          console.info('Sans video-fidelity v23 applied.');
+        }
+        return result;
+      };
+      chained.__sansV23Chained = true;
+    }
+  });
+})();
